8822aea4fea4c6717267c8491a50b255a3538ec3
Rees, Nathan <nrees@agrinw.com>
2018-02-23 21:34:42 -0800

    version bump 0.5.1

--- gpustat/__init__.py
+++ gpustat/__init__.py
@@ -2,12 +2,11 @@
 The gpustat module.
 """
 
-__version__ = '0.5.0.dev1'
+__version__ = '0.5.1'
 
+from .__main__ import print_gpustat, main
 from .core import GPUStat, GPUStatCollection
 from .core import new_query
-from .__main__ import print_gpustat, main
-
 
 __all__ = (
     '__version__',
